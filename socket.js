722b500b54e8be5cb43ef9387c5bc6bc64be0deb
chamchimayo <chamchi.maayo@gmail.com>
2022-11-10 01:42:37 +0900

    오전 1시 42분

--- socket.js
+++ socket.js
@@ -41,7 +41,7 @@ const MAXIMUM = 5
 //     }
 //     await roomHistory.save()
 // }
-router.post("/room/:roomName", authMiddleware, (req,res) => {
+router.post("/room/:roomName", (req,res) => {
     io.on('connection', (socket) => {
     let myRoomName = null
     // let email = null
